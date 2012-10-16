5ddf16fd3b7ea41e239225444af574ff17fd0ba1
Doridian Draconia <mriq91@gmail.com>
2012-10-16 19:19:23 +0200

    Hide ads for PRO

--- pages/view.lua
+++ pages/view.lua
@@ -12,7 +12,7 @@ end
 nameregex = nameregex[1]
 
 local database = ngx.ctx.database
-local file = database:query("SELECT f.fileid, f.thumbnail, f.type, f.name, u.username, f.time, f.extension, f.size FROM files AS f, users AS u WHERE f.fileid = '"..nameregex.."' AND f.user = u.id LIMIT 0,1")
+local file = database:query("SELECT f.fileid, f.thumbnail, f.type, f.name, u.username, u.pro_expiry, f.time, f.extension, f.size FROM files AS f, users AS u WHERE f.fileid = '"..nameregex.."' AND f.user = u.id LIMIT 0,1")
 if (not file) or (not file[1])  then
 	ngx.status = 404
 	ngx.print("File not found")
@@ -21,10 +21,5 @@ end
 file = file[1]
 
 dofile("scripts/navtbl.lua")
---[[table.insert(navtbl, {
-	url = "",
-	title = "View file",
-	active = true
-})]]
-ngx.print(load_template("view", {MAINTITLE = "View file - " .. file.name, ADDLINKS = build_nav(navtbl), FILE = file}))
+ngx.print(load_template("view", {MAINTITLE = "View file - " .. file.name, ADDLINKS = build_nav(navtbl), FILE = file, HIDE_ADS = (file.pro_expiry >= ngx.time())}))
 ngx.eof()
