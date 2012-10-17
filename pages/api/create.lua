c75c4a1a50e911dd1535c3575b1a99150c3b36bb
Doridian Draconia <mriq91@gmail.com>
2012-10-17 19:31:28 +0200

    Unified API login

--- pages/api/create.lua
+++ pages/api/create.lua
@@ -1,18 +1,5 @@
 dofile("/var/www/doripush/scripts/global.lua")
-
-if not ngx.ctx.user then
-	local user = ngx.var.http_X_Foxcaves_User
-	local pw = ngx.var.http_X_Foxcaves_Password
-	local success = false
-	if user and pw then
-		success = (ngx.ctx.login(user,pw,true) == ngx.ctx.LOGIN_SUCCESS)
-	end
-	if not success then
-		ngx.status = 403
-		ngx.print("Wrong username/password")
-		return ngx.eof()
-	end
-end
+dofile("scripts/api_login.lua")
 
 local database = ngx.ctx.database
 
---
+++ scripts/api_login.lua
@@ -0,0 +1,13 @@
+if not ngx.ctx.user then
+	local user = ngx.var.http_X_Foxcaves_User
+	local pw = ngx.var.http_X_Foxcaves_Password
+	local success = false
+	if user and pw then
+		success = (ngx.ctx.login(user,pw,true) == ngx.ctx.LOGIN_SUCCESS)
+	end
+	if not success then
+		ngx.status = 403
+		ngx.print("Wrong username/password")
+		return ngx.eof()
+	end
+end
