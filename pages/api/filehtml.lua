ab02a1d4be0f0d0be779544381338d32516bd890
Doridian Draconia <mriq91@gmail.com>
2012-10-21 20:54:51 +0200

    Better uploader, now refreshes file on failure

--- pages/api/filehtml.lua
+++ pages/api/filehtml.lua
@@ -4,7 +4,7 @@ dofile("scripts/api_login.lua")
 local database = ngx.ctx.database
 local file = database:query("SELECT * FROM files WHERE fileid = '"..database:escape(ngx.var.query_string).."' AND user = '"..ngx.ctx.user.id.."'")
 
-if not file then
+if not (file and file[1]) then
 	ngx.print("-")
 	return ngx.eof()
 end
