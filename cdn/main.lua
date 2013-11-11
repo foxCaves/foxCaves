c57a8ff05d69a71b24781b1508a898d6af1e038b
Doridian Draconia <mriq91@gmail.com>
2013-11-11 21:01:58 +0100

    Fixes

--- cdn/main.lua
+++ cdn/main.lua
@@ -1,5 +1,7 @@
 function send_file(disposition_type)
-	local file = file_get(ngx.var.fileid)
+	local fileid = ngx.var.fileid
+	local file = file_get(fileid)
+	
 	if (not file) or file.extension:sub(2):lower() ~= ngx.var.extension:lower() then
 		ngx.status = 404
 		ngx.print("File not found")
@@ -8,5 +10,5 @@ function send_file(disposition_type)
 	
 	ngx.header["Content-Dispotition"] = disposition_type .. "; filename=" .. file.name .. file.extension
 	
-	ngx.req.set_uri("/rawget/" .. file.fileid .. "/file" .. file.extension)
+	ngx.req.set_uri("/rawget/" .. fileid .. "/file" .. file.extension)
 end
