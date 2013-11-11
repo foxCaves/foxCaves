77ee54ee63bc25d663e2da8a5a40bbfd6d9c8d7b
Doridian Draconia <mriq91@gmail.com>
2013-11-11 21:07:25 +0100

    More CType

--- cdn/download.lua
+++ cdn/download.lua
@@ -1,4 +1,5 @@
 dofile(ngx.var.main_root .. "/scripts/global.lua")
 dofile("scripts/fileapi.lua")
 dofile("cdn/main.lua")
+ngx.header["Content-Type"] = "application/octet-stream"
 return send_file("attachment")
