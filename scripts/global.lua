d2b85f1b739de58345c701ada97701e3e2d830d0
Doridian Draconia <mriq91@gmail.com>
2013-11-12 13:52:30 +0100

    Remove WS for real

--- scripts/global.lua
+++ scripts/global.lua
@@ -119,7 +119,7 @@ function ngx.ctx.get_version()
 	local fh = io.open(".revision", "r")
 	local ret = fh:read("*all")
 	fh:close()
-	return ret:gsub("%s+", "")
+	return ret:gsub("[\r\n\t ]+", "")
 end
 
 function raw_push_action(action)
