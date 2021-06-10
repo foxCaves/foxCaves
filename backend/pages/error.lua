d44de1b1f0b824b7b5cccac1e0f0b6fcdd581275
Mark Dietzer <git@doridian.net>
2021-06-10 15:01:34 -0700

    allow /error to only trigger 4xx and 5xx codes

--- backend/pages/error.lua
+++ backend/pages/error.lua
@@ -2,6 +2,10 @@ dofile(ngx.var.main_root .. "/scripts/global.lua")
 
 local error_num = tonumber(ngx.var.path_element or "400")
 
+if error_num < 400 or error_num > 599 then
+    error_num = 500
+end
+
 ngx.status = error_num
 
 printTemplateAndClose("error", {MAINTITLE = "Error " .. error_num, ERROR_NUM = error_num})
