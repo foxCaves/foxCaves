e7c81320f2aa7c7ae51e3250744f272d276f056e
Mark Dietzer <git@doridian.net>
2021-06-09 19:31:45 -0700

    with ID and Session

--- backend/pages/api/livedraw.lua
+++ backend/pages/api/livedraw.lua
@@ -4,6 +4,14 @@ dofile("scripts/api_login.lua")
 
 local WS_URL = ngx.re.gsub(MAIN_URL, "^http", "ws", "o")
 
+local id = ngx.var.arg_id
+local session = ngx.var.arg_session
+
+if not id or not session then
+    ngx.exit(400)
+    return
+end
+
 ngx.print(cjson.encode({
-    url = WS_URL .. "/api/livedraw_ws"
+    url = WS_URL .. "/api/livedraw_ws?id=" .. id .. "&session=" .. session
 }))
