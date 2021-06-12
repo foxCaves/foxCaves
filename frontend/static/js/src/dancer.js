29f1a0b27ff60ead329f0b687a81904497ee32ae
Mark Dietzer <git@doridian.net>
2021-06-12 10:53:57 -0700

    use real inheritance

--- frontend/static/js/src/dancer.js
+++ frontend/static/js/src/dancer.js
@@ -738,41 +738,41 @@ class FourierTransform {
 
 		this.peakBand = 0;
 		this.peak = 0;
+	}
 
-		/**
-		 * Calculates the *middle* frequency of an FFT band.
-		 *
-		 * @param {Number} index The index of the FFT band.
-		 *
-		 * @returns The middle frequency in Hz.
-		 */
-		this.getBandFrequency = function (index) {
-			return this.bandwidth * index + this.bandwidth / 2;
-		};
+	/**
+	 * Calculates the *middle* frequency of an FFT band.
+	 *
+	 * @param {Number} index The index of the FFT band.
+	 *
+	 * @returns The middle frequency in Hz.
+	 */
+	getBandFrequency(index) {
+		return this.bandwidth * index + this.bandwidth / 2;
+	}
 
-		this.calculateSpectrum = function () {
-			var spectrum = this.spectrum,
-				real = this.real,
-				imag = this.imag,
-				bSi = 2 / this.bufferSize,
-				sqrt = Math.sqrt,
-				rval,
-				ival,
-				mag;
+	calculateSpectrum() {
+		var spectrum = this.spectrum,
+			real = this.real,
+			imag = this.imag,
+			bSi = 2 / this.bufferSize,
+			sqrt = Math.sqrt,
+			rval,
+			ival,
+			mag;
 
-			for (var i = 0, N = bufferSize / 2; i < N; i++) {
-				rval = real[i];
-				ival = imag[i];
-				mag = bSi * sqrt(rval * rval + ival * ival);
+		for (var i = 0, N = this.bufferSize / 2; i < N; i++) {
+			rval = real[i];
+			ival = imag[i];
+			mag = bSi * sqrt(rval * rval + ival * ival);
 
-				if (mag > this.peak) {
-					this.peakBand = i;
-					this.peak = mag;
-				}
-
-				spectrum[i] = mag;
+			if (mag > this.peak) {
+				this.peakBand = i;
+				this.peak = mag;
 			}
-		};
+
+			spectrum[i] = mag;
+		}
 	}
 }
 
@@ -785,9 +785,9 @@ class FourierTransform {
  *
  * @constructor
  */
-class FFT {
+class FFT extends FourierTransform {
 	constructor(bufferSize, sampleRate) {
-		FourierTransform.constructor.apply(this, bufferSize, sampleRate);
+		super(bufferSize, sampleRate);
 
 		this.reverseTable = new Uint32Array(bufferSize);
 
