b8f267fc22959e099c6fbe9068f05dfebdb514a5
Mark Dietzer <git@doridian.net>
2021-06-16 12:29:24 -0700

    a

--- backend/scripts/api_login.lua
+++ backend/scripts/api_login.lua
@@ -2,6 +2,8 @@ if not ngx.ctx.user then
 	local user, pw = parse_authorization_header(ngx.var.http_authorization)
 	local success = false
 	if user and pw then
+		ngx.log(ngx.ERR, "U: " .. user)
+		ngx.log(ngx.ERR, "P: " .. pass)
 		success = (ngx.ctx.login(user, pw, { nosession = true, login_with_apikey = true }) == ngx.ctx.LOGIN_SUCCESS)
 	end
 	if not ALLOW_GUEST and ((not success) or (not ngx.ctx.user)) then
