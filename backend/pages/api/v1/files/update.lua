d8ae377d26b98e802c1cb93ce626f9b74f84979b
Mark Dietzer <git@doridian.net>
2021-06-16 15:53:25 -0700

    Add file rename via PUT

---
+++ backend/pages/api/v1/files/update.lua
@@ -0,0 +1,30 @@
+-- ROUTE:PUT:/api/v1/files/{id}
+dofile(ngx.var.main_root .. "/scripts/global.lua")
+dofile("scripts/api_login.lua")
+if not ngx.ctx.user then return end
+
+local database = ngx.ctx.database
+
+dofile("scripts/fileapi.lua")
+local file = file_get(ngx.ctx.route_vars.id, ngx.ctx.user.id)
+if not file then
+    ngx.exit(404)
+    return
+end
+
+local newname = ngx.ctx.var.arg_name
+
+if newname:sub(newname:len() - file.extension:len()) ~= file.extension then
+    ngx.status = 400
+    ngx.print("Extension mismatch. Cannot change extension in rename")
+    ngx.eof()
+    return
+end
+
+file.name = newname
+
+database:hset(database.KEYS.FILES .. file.id, "name", newname)
+
+ngx.header["Content-Type"] = "application/json"
+ngx.print(cjson.encode(file))
+ngx.eof()
