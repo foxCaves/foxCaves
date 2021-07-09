21fea8ac63c971108ad86275daecf080571db8c8
Mark Dietzer <git@doridian.net>
2021-07-09 11:46:31 -0700

    rm messages and such

--- backend/pages/myfiles.lua
+++ backend/pages/myfiles.lua
@@ -2,4 +2,4 @@
 dofile(ngx.var.main_root .. "/scripts/global.lua")
 if not ngx.ctx.user then return ngx.redirect("/login") end
 
-printTemplateAndClose("myfiles", {MAINTITLE = "My files", MESSAGE = message})
+printTemplateAndClose("myfiles", {MAINTITLE = "My files"})
