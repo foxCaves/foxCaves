5b4c8eaf9f16b310aa2aacf7077821f87f53add7
Mark Dietzer <git@doridian.net>
2021-07-15 22:50:50 -0700

    rm useless code

--- backend/pages/mylinks.lua
+++ backend/pages/mylinks.lua
@@ -1,5 +1,4 @@
 -- ROUTE:GET:/mylinks
 dofile(ngx.var.main_root .. "/scripts/global.lua")
-if not ngx.ctx.user then return ngx.redirect("/login") end
 
 printTemplateAndClose("mylinks", {MAINTITLE = "My links"})
