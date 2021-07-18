9a27ffa87ef94c73ca9f2ae4dcfb3370b32a427c
Mark Dietzer <git@doridian.net>
2021-07-17 21:33:15 -0700

    rm error route

--- backend/pages/legal.lua
+++ backend/pages/legal.lua
@@ -3,7 +3,7 @@ dofile(ngx.var.main_root .. "/scripts/global.lua")
 
 local site = ngx.ctx.route_vars.path
 if site ~= "terms_of_service" and site ~= "privacy_policy" then
-	return ngx.exec("/error/404")
+	return ngx.exit(404)
 end
 
 printStaticTemplateAndClose(site, {})
