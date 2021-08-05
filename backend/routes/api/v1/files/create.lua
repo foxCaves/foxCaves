b0d586a22f990d7d5bd7728a4bf26e14b6765d0d
Mark Dietzer <git@doridian.net>
2021-08-04 22:58:29 -0700

    ugh

--- backend/routes/api/v1/files/create.lua
+++ backend/routes/api/v1/files/create.lua
@@ -130,7 +130,7 @@ local fileType, thumbnailType, thumbnail = mimeHandlers[prefix](suffix)
 
 file_upload(fileid, name, extension, thumbnail, mtype, thumbnailType)
 
-database:query_safe('INSERT INTO files (id, name, user, extension, type, size, time, thumbnail) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)', fileid, name, ngx.ctx.user.id, extension, fileType, filesize, ngx.time(), thumbnail or "")
+database:query_safe('INSERT INTO files (id, name, "user", extension, type, size, time, thumbnail) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)', fileid, name, ngx.ctx.user.id, extension, fileType, filesize, ngx.time(), thumbnail or "")
 ngx.ctx.user.usedbytes = ngx.ctx.user.usedbytes + filesize
 
 local filedata = file_get(fileid)
