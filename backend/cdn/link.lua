6874d41fbd1c53562d402473dfc54a18f6e59b98
Mark Dietzer <git@doridian.net>
2021-08-05 10:17:04 -0700

    f

--- backend/cdn/link.lua
+++ backend/cdn/link.lua
@@ -5,6 +5,8 @@ local database = ngx.ctx.database
 local dest = database:query_safe('SELECT url FROM links WHERE id = %s', linkid)
 dest = dest[1]
 
+ngx.header["Content-Type"] = "text/plain"
+
 if not dest then
     ngx.status = 404
     return
