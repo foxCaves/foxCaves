860438655c5219bc6ae13585b32c690844e33b83
Mark Dietzer <git@doridian.net>
2021-08-05 10:15:33 -0700

    wut

--- backend/cdn/link.lua
+++ backend/cdn/link.lua
@@ -3,11 +3,13 @@ ctx_init()
 local linkid = ngx.var.linkid
 local database = ngx.ctx.database
 local dest = database:query_safe('SELECT url FROM links WHERE id = %s', linkid)
-if not dest[1] then
+dest = dest[1]
+
+if not dest then
     ngx.status = 404
     return
 end
 
 __on_shutdown()
 ngx.status = 302
-ngx.redirect(dest[1].url)
+ngx.redirect(dest.url)
--- backend/cdn/sendfile.lua
+++ backend/cdn/sendfile.lua
@@ -1,8 +1,7 @@
 ctx_init()
 
 local function send_file(disposition_type)
-	local fileid = ngx.var.fileid
-	local file = file_get_public(fileid)
+	local file = file_get_public(ngx.var.fileid)
 
 	if (not file) or file.extension:sub(2):lower() ~= ngx.var.extension:lower() then
 		ngx.status = 404
@@ -13,7 +12,7 @@ local function send_file(disposition_type)
 	ngx.header["Content-Dispotition"] = disposition_type .. "; filename=" .. file.name
 
 	__on_shutdown()
-	ngx.req.set_uri("/rawget/" .. fileid .. "/file" .. file.extension, true)
+	ngx.req.set_uri("/rawget/" .. file.id .. "/file" .. file.extension, true)
 end
 
 if ngx.var.action == "f" then
