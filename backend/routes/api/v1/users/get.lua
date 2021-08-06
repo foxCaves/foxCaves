5e6b5c3c3a86f4a7bff8bf44d71c183440119a2f
Mark Dietzer <git@doridian.net>
2021-08-05 19:02:47 -0700

    wat

--- backend/routes/api/v1/users/get.lua
+++ backend/routes/api/v1/users/get.lua
@@ -1,12 +1,12 @@
 -- ROUTE:GET:/api/v1/users/{id}
 ctx_init()
 
-local database = ngx.ctx.database
-
-local userres = database:query_safe('SELECT id, username FROM users WHERE id = %s', ngx.ctx.route_vars.id)
+local userres = ngx.ctx.database:query_safe('SELECT id, username FROM users WHERE id = %s', ngx.ctx.route_vars.id)
 local user = userres[1]
 if not user then
     ngx.status = 404
     return
 end
 ngx.print(cjson.encode(user))
+
+ngx.log(ngx.ERR, "meow!")
