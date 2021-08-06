5c282c8033e7775cca8a4e2ebeacd169f82d2a0f
Mark Dietzer <git@doridian.net>
2021-08-05 22:34:12 -0700

    split out main and init

---
+++ backend/core/main.lua
@@ -0,0 +1,99 @@
+ENVIRONMENT = os.getenv("ENVIRONMENT") or "development"
+IS_PRODUCTION = (ENVIRONMENT == "production")
+INIT_COMPLETE = false
+
+local function load_revision()
+	local fh = io.open("/var/www/foxcaves/.revision", "r")
+	if not fh then
+		REVISION = "unknown"
+		return
+	end
+	REVISION = fh:read("*all"):gsub("%s+", "")
+	fh:close()
+end
+load_revision()
+
+function explode(div,str) -- credit: http://richard.warburton.it
+	local pos, arr = 0, {}
+	-- for each divider found
+	for st, sp in function() return str:find(div,pos,true) end do
+		table.insert(arr,str:sub(pos,st-1)) -- Attach chars left of current divider
+		pos = sp + 1 -- Jump past current divider
+	end
+	table.insert(arr, str:sub(pos)) -- Attach chars right of last divider
+	return arr
+end
+
+local filecache = {}
+function loadfile_cached(file)
+	local cached = filecache[file]
+	if cached then
+		return cached
+	end
+
+	local fh = io.open(file, "r")
+	if not fh then
+		error("Could not open file: " .. file)
+	end
+	local data = fh:read("*all")
+	fh:close()
+
+	local func, err = load(data, file)
+	if err then
+		error(err)
+	end
+
+	if IS_PRODUCTION then
+		filecache[file] = func
+		if INIT_COMPLETE then
+			ngx.log(ngx.ERR, "Caching Lua file during runtime in production: " .. file)
+		end
+	end
+
+	return func
+end
+local loadfile_cached = loadfile_cached
+function dofile_cached(file)
+	local func = loadfile_cached(file)
+	return func()
+end
+
+local loadfile = loadfile
+function dofile(file)
+	loadfile(file)()
+end
+
+function parse_authorization_header(auth)
+	if not auth then
+		return
+	end
+	if auth:sub(1, 6):lower() ~= "basic " then
+		return
+	end
+	auth = ngx.decode_base64(auth:sub(7))
+	if not auth or auth == "" then
+		return
+	end
+	local colonPos = auth:find(":", 1, true)
+	if not colonPos then
+		return
+	end
+	return auth:sub(1, colonPos - 1), auth:sub(colonPos + 1)
+end
+
+local function scan_include_dir(dir)
+    for file in lfs.dir(dir) do
+        if file:sub(1, 1) ~= "." then
+            local absfile = dir .. "/" .. file
+            local attributes = lfs.attributes(absfile)
+            if attributes.mode == "file" then
+                loadfile(absfile)()
+            elseif attributes.mode == "directory" then
+                scan_include_dir(absfile)
+            end
+        end
+    end
+end
+scan_include_dir("core/includes")
+
+INIT_COMPLETE = true
