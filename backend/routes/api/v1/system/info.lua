92b3c89d1a22861bed9e4a59c30542902edc1e50
Mark Dietzer <git@doridian.net>
2021-08-07 14:27:22 -0700

    sentry state

--- backend/routes/api/v1/system/info.lua
+++ backend/routes/api/v1/system/info.lua
@@ -2,5 +2,6 @@ register_route("/api/v1/system/info", "GET", make_route_opts_anon(), function()
     return {
         environment = ENVIRONMENT,
         release = REVISION,
+        sentry = (SENTRY_DSN and SENTRY_DSN ~= ""),
     }
 end)
