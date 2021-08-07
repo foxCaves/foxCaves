5cfcb4710ab56115b87dcd409f4c305bf51a2a7b
Doridian <git@doridian.net>
2021-08-06 20:27:53 -0700

    fix link deletion

--- backend/routes/api/v1/links/delete.lua
+++ backend/routes/api/v1/links/delete.lua
@@ -1,12 +1,13 @@
 register_route("/api/v1/links/{id}", "DELETE", make_route_opts(), function()
-    local res = get_ctx_database():query_safe('DELETE FROM links WHERE id = %s AND "user" = %s', ngx.ctx.route_vars.id, ngx.ctx.user.id)
-
-    if res.affected_rows > 0 then
-        raw_push_action({
-            action = "link:delete",
-            link = linkinfo,
-        })
-    else
-        return api_error("Could not delete link", 400)
+    local link = link_get(ngx.ctx.route_vars.id, ngx.ctx.user.id)
+    if not link then
+        return api_error("Could not delete link", 400) 
     end
+    
+    get_ctx_database():query_safe('DELETE FROM links WHERE id = %s', link.id)
+
+    raw_push_action({
+        action = "link:delete",
+        link = link,
+    })
 end)
