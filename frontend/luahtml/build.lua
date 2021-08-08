e1f9c36c1995700ce710638803c30a9ffb915cae
Mark Dietzer <git@doridian.net>
2021-08-07 21:06:22 -0700

    t

--- frontend/luahtml/build.lua
+++ frontend/luahtml/build.lua
@@ -20,23 +20,23 @@ os.execute("mkdir -p '" .. DISTDIR .. "'")
 os.execute("mkdir '" .. DISTDIR .. "/legal'")
 os.execute("mkdir '" .. DISTDIR .. "/email'")
 
-local function scanTemplateDirInt(dir, basedirlen)
+local function scanTemplateDirInt(dir, ext, extlen, basedirlen)
     for file in lfs.dir(dir) do
         local first = file:sub(1, 1)
         if first ~= "." and first ~= "_" then
             local absfile = dir .. "/" .. file
             local attributes = lfs.attributes(absfile)
-            if attributes.mode == "file" then
-                storeTemplate(absfile:sub(basedirlen))
+            if attributes.mode == "file" and file:sub(file:len() - extlen) == ext then
+                storeTemplate(absfile:sub(1, absfile:len() - extlen):sub(basedirlen))
             elseif attributes.mode == "directory" then
-                scanTemplateDirInt(absfile, basedirlen)
+                scanTemplateDirInt(absfile, ext, extlen, basedirlen)
             end
         end
     end
 end
-local function scanTemplateDir(dir)
-    scanTemplateDirInt(dir, dir:len() + 2)
+local function scanTemplateDir(dir, ext)
+    scanTemplateDirInt(dir, ext, ext:len(), dir:len() + 2)
 end
-scanTemplateDir("templates")
+scanTemplateDir("templates", ".tpl")
 
 print("Done!")
