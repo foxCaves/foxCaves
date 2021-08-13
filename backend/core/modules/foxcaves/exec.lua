7551b970ec58740ee72fdc6d0259a6233de38dcb
Doridian <git@doridian.net>
2021-08-13 09:50:39 -0700

    Try using ngx pipe

--- backend/core/modules/foxcaves/exec.lua
+++ backend/core/modules/foxcaves/exec.lua
@@ -1,16 +1,14 @@
-local resty_exec = require("resty.exec")
-
-local os = os
+local ngx_pipe = require("ngx.pipe")
 
 local M = {}
 setfenv(1, M)
 
 function M.cmd(...)
-    local sock = os.tmpname()
-    local prog = resty_exec.new(sock)
-    local res, err = prog(...)
-    os.remove(sock)
-    return res, err
+    local proc, err = ngx_pipe.spawn({...})
+    if err or not proc then
+        return false, err, -1
+    end
+    return proc:wait()
 end
 
 return M
