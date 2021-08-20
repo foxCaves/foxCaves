e728b12ca65f49d2814ebf8c4e8b1d0d2574296b
Mark Dietzer <git@doridian.net>
2021-08-20 12:24:49 -0700

    a

--- backend/modules/foxcaves/auth.lua
+++ backend/modules/foxcaves/auth.lua
@@ -116,9 +116,8 @@ function M.check_cookies()
 
 	local loginkey = cookie:get("loginkey")
 	if loginkey then
-		ngx.log(ngx.ERR, "lk: " .. tostring(loginkey))
 		if not ngx.ctx.user then
-			local loginkey_match = ngx.re.match(cookies, "^([0-9a-f-]+)\\.([a-zA-Z0-9+/=]+)$", "o")
+			local loginkey_match = ngx.re.match(loginkey, "^([0-9a-f-]+)\\.([a-zA-Z0-9+/=]+)$", "o")
 			if loginkey_match then
 				ngx.log(ngx.ERR, "lkm1: " .. tostring(loginkey_match[1]))
 				ngx.log(ngx.ERR, "lkm2: " .. tostring(loginkey_match[2]))
