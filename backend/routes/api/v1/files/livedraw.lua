4ef886409368ef5c6b26d3fe97dca16c051ee00b
Mark Dietzer <git@doridian.net>
2021-08-22 12:38:17 -0700

    a

--- backend/routes/api/v1/files/livedraw.lua
+++ backend/routes/api/v1/files/livedraw.lua
@@ -4,14 +4,12 @@ local WS_URL = ngx.re.gsub(require("foxcaves.config").urls.main, "^http", "ws", 
 local ngx = ngx
 
 R.register_route("/api/v1/files/{id}/livedraw", "GET", R.make_route_opts({ allow_guest = true }), function(route_vars)
-    local id = route_vars.id
     local session = ngx.var.arg_session
-
-    if not id or not session then
-        return utils.api_error("Missing id or session", 400)
+    if not session then
+        return utils.api_error("Missing session", 400)
     end
 
     return {
-        url = WS_URL .. "/api/v1/ws/livedraw?id=" .. id .. "&session=" .. session
+        url = WS_URL .. "/api/v1/ws/livedraw?id=" .. route_vars.id .. "&session=" .. session
     }
 end)
