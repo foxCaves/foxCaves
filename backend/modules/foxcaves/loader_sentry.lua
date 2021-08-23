b21f797909bd2d62b9096f28b0b311a0c059247c
Mark Dietzer <git@doridian.net>
2021-08-22 22:48:28 -0700

    split out loaders and only load the one we need

---
+++ backend/modules/foxcaves/loader_sentry.lua
@@ -0,0 +1,38 @@
+local utils = require("foxcaves.utils")
+local router = require("foxcaves.router")
+local raven = require("raven")
+local raven_sender = require("raven.senders.ngx")
+local env = require("foxcaves.env")
+local revision = require("foxcaves.revision")
+local sentry_config = require("foxcaves.config").sentry
+local ngx = ngx
+
+local M = {}
+require("foxcaves.module_helper").setmodenv()
+
+local rvn = raven.new({
+    sender = raven_sender.new({
+        dsn = sentry_config.dsn,
+    }),
+    environment = env.name,
+    release = revision.hash,
+})
+
+function M.run()
+    local isok, err = rvn:call_ext({
+        user = ngx.ctx.user and ngx.ctx.user:get_public(),
+        tags = {
+            ip = ngx.var.remote_addr,
+            url = ngx.var.request_uri,
+        },
+    }, router.execute)
+    ngx.req.discard_body()
+    if not isok then
+        ngx.status = 500
+        ngx.log(ngx.ERR, "Lua error: " .. err)
+    end
+    utils.__on_shutdown()
+    ngx.eof()
+end
+
+return M
