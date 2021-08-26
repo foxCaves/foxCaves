543adcd0dc8bc814c66582c9c9a3f3e06d2f8b15
Mark Dietzer <git@doridian.net>
2021-08-26 10:13:14 -0700

    Add basic pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
@@ -6,7 +6,10 @@ import Nav from 'react-bootstrap/Nav';
 import Alert from 'react-bootstrap/Alert';
 import { HomePage } from './pages/home';
 import { LoginPage } from './pages/login';
+import { RegistrationPage } from './pages/register';
 import { FilesPage } from './pages/files';
+import { LinksPage } from './pages/links';
+import { AccountPage } from './pages/account';
 import React from 'react';
 import { User } from './models/user';
 import { AppContext, AppContextClass } from './utils/context';
@@ -65,18 +68,21 @@ export class App extends React.Component<{}, AppState> {
         if (this.state.userLoaded) {
             if (this.state.user) {
                 nav = (<>
-                    <LinkContainer to="/"><Nav.Link>Home</Nav.Link></LinkContainer>
+                    <LinkContainer to="/" exact><Nav.Link>Home</Nav.Link></LinkContainer>
                     <LinkContainer to="/files"><Nav.Link>Files</Nav.Link></LinkContainer>
+                    <LinkContainer to="/links"><Nav.Link>Links</Nav.Link></LinkContainer>
+                    <LinkContainer to="/account"><Nav.Link>Account</Nav.Link></LinkContainer>
                 </>);
             } else {
                 nav = (<>
-                    <LinkContainer to="/"><Nav.Link>Home</Nav.Link></LinkContainer>
+                    <LinkContainer to="/" exact><Nav.Link>Home</Nav.Link></LinkContainer>
                     <LinkContainer to="/login"><Nav.Link>Login</Nav.Link></LinkContainer>
+                    <LinkContainer to="/register"><Nav.Link>Login</Nav.Link></LinkContainer>
                 </>);
             }
         } else {
             nav = (<>
-                <LinkContainer to="/"><Nav.Link>Home</Nav.Link></LinkContainer>
+                <LinkContainer to="/" exact><Nav.Link>Home</Nav.Link></LinkContainer>
             </>);
         }
 
@@ -111,12 +117,24 @@ export class App extends React.Component<{}, AppState> {
                             <CustomRoute path="/login" login={LoginState.LoggedOut}>
                                 <LoginPage />
                             </CustomRoute>
+                            <CustomRoute path="/register" login={LoginState.LoggedOut}>
+                                <RegistrationPage />
+                            </CustomRoute>
                             <CustomRoute path="/files" login={LoginState.LoggedIn}>
                                 <FilesPage />
                             </CustomRoute>
-                            <CustomRoute path="/">
-                                <HomePage />
+                            <CustomRoute path="/links" login={LoginState.LoggedIn}>
+                                <LinksPage />
                             </CustomRoute>
+                            <CustomRoute path="/account" login={LoginState.LoggedIn}>
+                                <AccountPage />
+                            </CustomRoute>
+                            <Route path="/" exact>
+                                <HomePage />
+                            </Route>
+                            <Route path="/">
+                                <h3>404 - Page not found</h3>
+                            </Route>
                         </Switch>
                     </Container>
                 </Router>
@@ -124,4 +142,3 @@ export class App extends React.Component<{}, AppState> {
         );
     }
 }
-
---
+++ frontend/src/pages/links.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class LinksPage extends React.Component<{}, {}> {
+    render() {
+        return (
+            <div>
+                <h1>Manage links</h1>
+                <p>TODO</p>
+            </div>
+        );
+    }
+}
