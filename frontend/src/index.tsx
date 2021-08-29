3453af1a94cc369ac11a0e63e2c007155c4d7230
Mark Dietzer <git@doridian.net>
2021-08-29 14:20:48 -0700

    Add sentry support

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,22 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/react';
 import { App } from './app';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootswatch/dist/vapor/bootstrap.min.css';
+
+declare const CONFIG: {
+    sentry_dsn: string;
+};
+
+if (process.env.NODE_ENV === 'production') {
+    Sentry.init({
+        dsn: CONFIG.sentry_dsn,
+    });
+} else {
+    console.warn(`Running in ${process.env.NODE_ENV} mode. Not loading sentry`);
+}
 
 ReactDOM.render(
     <StrictMode>
