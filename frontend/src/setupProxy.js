52747a140d4f7fb3d2f95939a3b3b3f46fe6c259
Mark Dietzer <git@doridian.net>
2021-08-29 16:32:53 -0700

    add comment

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,5 +1,8 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+// DO NOT CHANGE THIS FILE TO .TS
+// webpack-dev-server / create-react-app do not support .ts files currently
+
 module.exports = function (app) {
     app.use(createProxyMiddleware('/api/v1/ws', {
         target: 'https://foxcav.es',
