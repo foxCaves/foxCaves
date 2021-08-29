c243f5aa9ad1871ac8ac2dda0c043dd4aa9ee723
Mark Dietzer <git@doridian.net>
2021-08-29 00:25:48 -0700

    optimize logout

--- frontend/src/pages/logout.tsx
+++ frontend/src/pages/logout.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useState, useEffect, useCallback } from 'react';
 import { fetchAPIRaw } from '../utils/api';
 import { AppContext } from '../utils/context';
 import { Redirect } from 'react-router-dom';
@@ -8,43 +8,44 @@ export const LogoutPage: React.FC = () => {
     const [logoutDone, setLogoutDone] = useState(false);
     const [logoutStarted, setLogoutStarted] = useState(false);
 
+    const logoutAPI = useCallback(async () => {
+        try {
+            await fetchAPIRaw('/api/v1/users/sessions/logout', {
+                method: 'POST',
+            });
+        } catch (err: any) {
+            showAlert({
+                id: 'logout',
+                contents: `Error logging out: ${err.message}`,
+                variant: 'danger',
+                timeout: 5000,
+            });
+            return;
+        }
+
+        showAlert({
+            id: 'logout',
+            contents: 'Logged out!',
+            variant: 'success',
+            timeout: 5000,
+        });
+    }, [showAlert]);
+
+    const logout = useCallback(async () => {
+        setLogoutStarted(true);
+        await logoutAPI();
+        refreshUser();
+        setLogoutDone(true);
+        setLogoutStarted(false);
+    }, [logoutAPI, refreshUser]);
+
     useEffect(() => {
         if (logoutDone || logoutStarted) {
             return;
         }
 
-        async function logoutAPI() {
-            try {
-                await fetchAPIRaw('/api/v1/users/sessions/logout', {
-                    method: 'POST',
-                });
-            } catch (err: any) {
-                showAlert({
-                    id: 'logout',
-                    contents: `Error logging out: ${err.message}`,
-                    variant: 'danger',
-                    timeout: 5000,
-                });
-                return;
-            }
-
-            showAlert({
-                id: 'logout',
-                contents: 'Logged out!',
-                variant: 'success',
-                timeout: 5000,
-            });
-        }
-
-        async function logout() {
-            setLogoutStarted(true);
-            await logoutAPI();
-            refreshUser();
-            setLogoutDone(true);
-            setLogoutStarted(false);
-        }
         logout();
-    }, [showAlert, refreshUser, logoutDone, logoutStarted]);
+    }, [logoutDone, logoutStarted, logout]);
 
     if (logoutDone) {
         return <Redirect to="/" />;
