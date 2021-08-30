6de0c7117692024b8d694aefb3a550a25c70db32
Mark Dietzer <git@doridian.net>
2021-08-30 08:24:54 -0700

    Add more RESTful files and links lister

---
+++ backend/routes/api/v1/users/files.lua
@@ -0,0 +1,21 @@
+local file_model = require("foxcaves.models.file")
+local user_model = require("foxcaves.models.user")
+local utils = require("foxcaves.utils")
+local ngx = ngx
+local next = next
+
+R.register_route("/api/v1/users/{id}/files", "GET", R.make_route_opts({ empty_is_array = true }), function(route_vars)
+    local user = user_model.get_by_id(route_vars.id)
+    if not user then
+        return utils.api_error("User not found", 404)
+    end
+    if user.id ~= ngx.ctx.user.id then
+        return utils.api_error("You are not list files for this user", 403)
+    end
+
+    local res = file_model.get_by_user(user)
+    for k, v in next, res do
+        res[k] = v:get_public()
+    end
+    return res
+end)
---
+++ backend/routes/api/v1/users/links.lua
@@ -0,0 +1,21 @@
+local link_model = require("foxcaves.models.link")
+local user_model = require("foxcaves.models.user")
+local utils = require("foxcaves.utils")
+local ngx = ngx
+local next = next
+
+R.register_route("/api/v1/users/{id}/links", "GET", R.make_route_opts({ empty_is_array = true }), function(route_vars)
+    local user = user_model.get_by_id(route_vars.id)
+    if not user then
+        return utils.api_error("User not found", 404)
+    end
+    if user.id ~= ngx.ctx.user.id then
+        return utils.api_error("You are not list links for this user", 403)
+    end
+
+    local res = link_model.get_by_user(user)
+    for k, v in next, res do
+        res[k] = v:get_public()
+    end
+    return res
+end)
