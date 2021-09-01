2b56e11dc2130b8c9948d73214cf725a41fbc928
Mark Dietzer <git@doridian.net>
2021-09-01 10:07:08 -0700

    since we cache this anyway, no need to optimize here

--- backend/routes/api/v1/system/describe.lua
+++ backend/routes/api/v1/system/describe.lua
@@ -1,68 +1,65 @@
-local ROUTE_TABLE = R.get_route_table()
+local get_route_table = R.get_route_table
 local next = next
 
-local TYPE_TABLE = {
-    ["string"] = {
-        type = "string",
-        description = "An arbitrary string",
-    },
-    ["float"] = {
-        type = "number",
-        description = "A floating-point number",
-    },
-    ["integer"] = {
-        type = "number",
-        description = "An integer number",
-    },
-    ["boolean"] = {
-        type = "boolean",
-        description = "A boolean value",
-    },
-    ["uuid"] = {
-        type = "string",
-        description = "A UUID in string form",
-    },
-    ["timestamp"] = {
-        type = "string",
-        description = "A timestamp in ISO-8601 format",
-    },
-    ["object"] = {
-        type = "object",
-        description = "An arbitrary object, containing given fields",
-        fields = {},
-    },
-    ["raw"] = {
-        type = "raw",
-        description = "A raw value, not encoded at all",
-    },
-    ["array"] = {
-        type = "array",
-        description = "An array of arbitrary values, described by items",
-        items = {},
-    },
-}
-local function load_model(m)
-    local model = require("foxcaves.models." .. m)
-    TYPE_TABLE[m .. ".public"] = {
-        type = "object",
-        fields = model.get_public_fields()
-    }
-    TYPE_TABLE[m .. ".private"] = {
-        type = "object",
-        fields = model.get_private_fields()
-    }
-end
-load_model("user")
-load_model("file")
-load_model("link")
 
 local function describe_api()
     local res = {
         routes = {},
-        types = TYPE_TABLE,
+        types = {
+            ["string"] = {
+                type = "string",
+                description = "An arbitrary string",
+            },
+            ["float"] = {
+                type = "number",
+                description = "A floating-point number",
+            },
+            ["integer"] = {
+                type = "number",
+                description = "An integer number",
+            },
+            ["boolean"] = {
+                type = "boolean",
+                description = "A boolean value",
+            },
+            ["uuid"] = {
+                type = "string",
+                description = "A UUID in string form",
+            },
+            ["timestamp"] = {
+                type = "string",
+                description = "A timestamp in ISO-8601 format",
+            },
+            ["object"] = {
+                type = "object",
+                description = "An arbitrary object, containing given fields",
+                fields = {},
+            },
+            ["raw"] = {
+                type = "raw",
+                description = "A raw value, not encoded at all",
+            },
+            ["array"] = {
+                type = "array",
+                description = "An array of arbitrary values, described by items",
+                items = {},
+            },
+        },
     }
 
-    for url, methods in next, ROUTE_TABLE do
+    for _, m in pairs({"user","file","link"}) do
+        local model = require("foxcaves.models." .. m)
+        res.types[m .. ".public"] = {
+            type = "object",
+            fields = model.get_public_fields()
+        }
+        res.types[m .. ".private"] = {
+            type = "object",
+            fields = model.get_private_fields()
+        }
+    end
+
+    for url, methods in next, get_route_table() do
         for method, route in next, methods do
             if route.descriptor and not route.descriptor.hidden then
                 if not res.routes[url] then
