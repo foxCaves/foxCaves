24c3251ac81c96132371d5b37e21ad18dc506b90
Mark Dietzer <git@doridian.net>
2022-02-27 11:49:51 -0800

    Add expiry cron

--- backend/routes/api/v1/system/expire.lua
+++ backend/routes/api/v1/system/expire.lua
@@ -5,4 +5,6 @@ local file_model = require("foxcaves.models.file")
 R.register_route("/api/v1/system/expire", "POST", R.make_route_opts_anon(), function()
     expiry_utils.delete_expired(link_model)
     expiry_utils.delete_expired(file_model)
+
+    return { ok = true }
 end)
