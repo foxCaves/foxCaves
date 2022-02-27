39ffe9035bcb171050f25e2fdbce82ad3690efc5
Mark Dietzer <git@doridian.net>
2022-02-27 11:28:02 -0800

    docs

--- backend/modules/foxcaves/expiry_utils.lua
+++ backend/modules/foxcaves/expiry_utils.lua
@@ -21,7 +21,7 @@ function M.parse_expiry(args, model, prefix)
     if expires_in then
         expires_in = tonumber(expires_in)
         if expires_in > 0 then
-            local res = database:query_single(
+            local res = database.get_shared():query_single(
                 "SELECT to_json(now() + INTERVAL %s SECOND at time zone \'utc\') as expires_at", expires_in)
             model.expires_at = res.expires_at
         end
