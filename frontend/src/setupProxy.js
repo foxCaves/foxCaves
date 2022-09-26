42f21fa2755ae371c56406822ab4397621348296
Doridian <git@doridian.net>
2022-09-26 08:48:49 -0700

    Real domains

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -6,14 +6,14 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
     app.use(
         createProxyMiddleware('/api/v1/ws', {
-            target: 'https://caves.fox.ax',
+            target: 'https://foxcav.es',
             ws: true,
             changeOrigin: true,
         }),
     );
     app.use(
         createProxyMiddleware('/api', {
-            target: 'https://caves.fox.ax',
+            target: 'https://foxcav.es',
             changeOrigin: true,
         }),
     );
