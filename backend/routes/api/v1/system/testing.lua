8a8e61cd42d947c82371c76b2922a5fd988173ad
Doridian <git@doridian.net>
2023-03-26 01:35:22 -0700

    Add error test endpoint

--- backend/routes/api/v1/system/testing.lua
+++ backend/routes/api/v1/system/testing.lua
@@ -1,7 +1,6 @@
-local enable_test_error = not not require("foxcaves.config").app.enable_test_error
 local error = error
 
-if enable_test_error then
+if require("foxcaves.config").app.enable_test_error then
     R.register_route("/api/v1/system/error", "GET", R.make_route_opts_anon(), function()
         error("test error")
     end)
