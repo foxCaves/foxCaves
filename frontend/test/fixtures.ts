bdc131808bda312e0c5b65774bdd9758afdbab50
Doridian <git@doridian.net>
2023-03-28 13:32:21 -0700

    Allow retrieving test user from test

--- frontend/test/fixtures.ts
+++ frontend/test/fixtures.ts
@@ -1,29 +1,49 @@
-import fs from 'node:fs';
+import { existsSync } from 'node:fs';
+import { mkdir, readFile, writeFile } from 'node:fs/promises';
 import path from 'node:path';
 import { test as baseTest } from '@playwright/test';
-import { makeLoggedInPage } from './utils';
+import { doLoginPage, TestUser } from './utils';
 
-export const test = baseTest.extend<object, { workerStorageState: string }>({
+async function getStoragePath(suffix: 'storage' | 'user'): Promise<string> {
+    const basePath = path.resolve(test.info().project.outputDir, '.auth');
+    try {
+        await mkdir(basePath, { recursive: true });
+    } catch {
+        // Ignore errors.
+    }
+
+    // Use parallelIndex as a unique identifier for each worker.
+    const id = test.info().parallelIndex;
+    return path.resolve(basePath, `${id}_${suffix}.json`);
+}
+
+export const test = baseTest.extend<{ testUser: TestUser }, { workerStorageState: string }>({
     // Use the same storage state for all tests in this worker.
     storageState: async ({ workerStorageState }, use) => {
         await use(workerStorageState);
     },
 
+    testUser: async () => {
+        return JSON.parse(await readFile(await getStoragePath('user'), 'utf8')) as TestUser;
+    },
+
     // Authenticate once per worker with a worker-scoped fixture.
     workerStorageState: [
         async ({ browser }, use) => {
-            // Use parallelIndex as a unique identifier for each worker.
-            const id = test.info().parallelIndex;
-            const fileName = path.resolve(test.info().project.outputDir, `.auth/${id}.json`);
+            const fileName = await getStoragePath('storage');
 
-            if (fs.existsSync(fileName)) {
+            if (existsSync(fileName)) {
                 // Reuse existing authentication state if any.
                 await use(fileName);
                 return;
             }
 
             // Important: make sure we authenticate in a clean environment by unsetting storage state.
-            const page = await makeLoggedInPage(browser);
+            const page = await browser.newPage({ storageState: undefined });
+            const user = await doLoginPage(page);
+
+            await writeFile(await getStoragePath('user'), JSON.stringify(user), 'utf8');
+
             await page.context().storageState({ path: fileName });
             await page.close();
             await use(fileName);
--- frontend/test/utils.ts
+++ frontend/test/utils.ts
@@ -1,32 +1,37 @@
 import { randomUUID } from 'node:crypto';
-import { Browser, Page } from '@playwright/test';
+import { Page } from '@playwright/test';
 
-export async function doLoginPage(page: Page): Promise<void> {
-    const username = `test_user_${randomUUID()}`;
-    const password = `test_password_${randomUUID()}`;
+export interface TestUser {
+    username: string;
+    password: string;
+}
+
+export async function doLoginPage(page: Page, user?: TestUser): Promise<TestUser> {
+    if (!user) {
+        user = {
+            username: `test_user_${randomUUID()}`,
+            password: `test_password_${randomUUID()}`,
+        };
+    }
 
     await page.goto('http://main.foxcaves:8080/register');
-    await page.locator('input[name="username"]').fill(username);
-    await page.locator('input[name="password"]').fill(password);
-    await page.locator('input[name="passwordConfirm"]').fill(password);
-    await page.locator('input[name="email"]').fill(`${username}@main.foxcaves`);
+    await page.locator('input[name="username"]').fill(user.username);
+    await page.locator('input[name="password"]').fill(user.password);
+    await page.locator('input[name="passwordConfirm"]').fill(user.password);
+    await page.locator('input[name="email"]').fill(`${user.username}@main.foxcaves`);
     await page.getByLabel('I agree to the Terms of Service and Privacy Policy').check();
     await page.getByRole('button').locator('text="Register"').click();
     await page.locator('.Toastify__toast--success').waitFor();
 
     await page.goto('http://main.foxcaves:8080/login');
-    await page.locator('input[name="username"]').fill(username);
-    await page.locator('input[name="password"]').fill(password);
+    await page.locator('input[name="username"]').fill(user.username);
+    await page.locator('input[name="password"]').fill(user.password);
     await page.getByLabel('Remember me').check();
     await page.getByRole('button').locator('text="Login"').click();
     await page.locator('.Toastify__toast--success').waitFor();
 
     await page.goto('http://main.foxcaves:8080/login');
     await page.locator('text="Home"').waitFor();
-}
 
-export async function makeLoggedInPage(browser: Browser): Promise<Page> {
-    const page = await browser.newPage({ storageState: undefined });
-    await doLoginPage(page);
-    return page;
+    return user;
 }
