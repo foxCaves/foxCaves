a613c0e272d3a662be4f6312b316f8bcffb79ae7
Doridian <git@doridian.net>
2023-04-10 23:12:54 -0700

    Update mime list

--- backend/scripts/mimelist.js
+++ backend/scripts/mimelist.js
@@ -8,6 +8,19 @@ const outfile = join(__dirname, '../modules/foxcaves/mimetypes.lua');
 
 const URL = 'https://cdn.jsdelivr.net/gh/jshttp/mime-db@master/db.json';
 
+const SIMPLE_KEY_REGEX = /^[a-z_][a-z0-9_]*$/;
+const LUA_KEYWORDS = new Set(['for', 'if', 'in', 'while']);
+
+function luaKeyEncode(key) {
+    key = key.toLowerCase();
+
+    if (SIMPLE_KEY_REGEX.test(key) && !LUA_KEYWORDS.has(key)) {
+        return key;
+    }
+
+    return `['${key}']`;
+}
+
 function parseMimes(mimes) {
     let extensionToMimeMap = {};
 
@@ -24,9 +37,9 @@ function parseMimes(mimes) {
     writeFileSync(
         outfile,
         'return {\n' +
-            Object.entries(extensionToMimeMap)
-                .map(([key, value]) => `\t[${JSON.stringify(key)}] = ${JSON.stringify(value)}`)
-                .join(',\n') +
+            Object.entries(extensionToMimeMap).sort((a, b) => a[0].localeCompare(b[0]))
+                .map(([key, value]) => `    ${luaKeyEncode(key)} = '${value}',`)
+                .join('\n') +
             '\n}\n',
     );
 }
