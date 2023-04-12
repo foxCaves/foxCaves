64e69dfa96679e705dd46d7bbc2efc6e011208fa
Mark Dietzer <git@doridian.net>
2023-04-12 15:54:42 -0700

    Prettify client_config (#306)

--- backend/routes/api/v1/system/client_config.lua
+++ backend/routes/api/v1/system/client_config.lua
@@ -3,27 +3,26 @@ local cjson = require('cjson')
 local ngx = ngx
 
 local config = {
-    captcha = {},
-    sentry = { dsn = nil },
-}
-
-for key, default_value in
-    pairs({
+    captcha = {
         registration = false,
         login = false,
         forgot_password = false,
         resend_activation = false,
         recaptcha_site_key = '',
-    })
-do
+    },
+    sentry = { dsn = '' },
+}
+
+for key, _ in pairs(config.captcha) do
     local value = config_raw.captcha[key]
-    if value == nil then
-        value = default_value
+    if value ~= nil then
+        config.captcha[key] = value
     end
-    config.captcha[key] = value
 end
 
-config.sentry.dsn = config_raw.sentry.dsn_frontend
+if config_raw.sentry.dsn_frontend ~= nil then
+    config.sentry.dsn = config_raw.sentry.dsn_frontend
+end
 
 R.register_route('/api/v1/system/client_config', 'GET', R.make_route_opts_anon(), function()
     return config
@@ -31,6 +30,6 @@ end)
 
 R.register_route('/api/v1/system/client_config.js', 'GET', R.make_route_opts_anon(), function()
     ngx.header['Content-Type'] = 'text/javascript'
-    ngx.say('window.foxcavesConfig = ' .. cjson.encode(config))
+    ngx.say("'use strict';\nwindow.foxcavesConfig = " .. cjson.encode(config) .. ';')
     ngx.eof()
 end)
