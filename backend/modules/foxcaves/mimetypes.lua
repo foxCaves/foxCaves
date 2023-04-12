995308ee4151485a296935402d3848945c5355c8
Doridian <git@doridian.net>
2023-04-11 23:48:23 -0700

    fmt

--- backend/modules/foxcaves/mimetypes.lua
+++ backend/modules/foxcaves/mimetypes.lua
@@ -2,30 +2,30 @@ local EXTENSION_TO_MIMETYPE = {}
 local MIMETYPE_TO_EXTENSIONS = {}
 
 local function load_mimetypes()
-    local fh = io.open("/usr/local/openresty/nginx/conf/mime.types", "r")
+    local fh = io.open('/usr/local/openresty/nginx/conf/mime.types', 'r')
 
-    local semicolon = (";"):byte(1)
+    local semicolon = (';'):byte(1)
     local in_mimetypes = false
     local current_mimetype_defs = {}
     while true do
-        local line = fh:read("*l")
+        local line = fh:read('*l')
         if not line then
             break
         end
 
         line = line:gsub('^%s+', ''):gsub('%s+$', ''):gsub('%s+', ' ')
-        if line == "types {" then
+        if line == 'types {' then
             in_mimetypes = true
-        elseif line == "}" then
+        elseif line == '}' then
             in_mimetypes = false
         elseif in_mimetypes then
-            for token in string.gmatch(line, "[^%s]+") do
+            for token in string.gmatch(line, '[^%s]+') do
                 local had_semicolon = token:byte(#token) == semicolon
                 if had_semicolon then
                     token = token:sub(1, #token - 1)
                 end
 
-                if token ~= "" then
+                if token ~= '' then
                     table.insert(current_mimetype_defs, token)
                 end
 
@@ -34,14 +34,14 @@ local function load_mimetypes()
                     if not MIMETYPE_TO_EXTENSIONS[mimetype] then
                         MIMETYPE_TO_EXTENSIONS[mimetype] = {}
                     end
-                    
+
                     for _, ext in pairs(current_mimetype_defs) do
                         EXTENSION_TO_MIMETYPE[ext] = mimetype
                         table.insert(MIMETYPE_TO_EXTENSIONS[mimetype], ext)
                     end
                     current_mimetype_defs = {}
                 end
-             end
+            end
         end
     end
     fh:close()
