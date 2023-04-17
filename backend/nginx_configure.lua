2351847b11f309921752b9c5178e56b524f7c0e4
Mark Dietzer <git@doridian.net>
2023-04-16 21:48:46 -0700

    Init randomseed per worker (#327)

--- backend/nginx_configure.lua
+++ backend/nginx_configure.lua
@@ -12,6 +12,7 @@ rawset(_G, 'ngx', {
 local path = require('path')
 local root = path.abs(debug.getinfo(1, 'S').source:sub(2):match('(.*/)'))
 dofile(root .. '/init.lua')
+dofile(root .. '/init_worker.lua')
 
 local config = require('foxcaves.config')
 
---
+++ backend/nginx_init_worker.lua
@@ -0,0 +1,3 @@
+local path = require('path')
+local root = path.abs(debug.getinfo(1, 'S').source:sub(2):match('(.*/)'))
+dofile(root .. '/init_worker.lua')
