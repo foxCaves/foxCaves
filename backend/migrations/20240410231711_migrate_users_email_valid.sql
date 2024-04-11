be98f225468c41b9bf71dc3437cc5cfffb617f3a
Mark Dietzer <git@doridian.net>
2024-04-11 00:06:01 -0700

    Fix user-list (#1060)

--- backend/migrations/20240410231711_migrate_users_email_valid.sql
+++ backend/migrations/20240410231711_migrate_users_email_valid.sql
@@ -1,3 +1,6 @@
 ALTER TABLE users RENAME COLUMN active TO email_valid;
 ALTER TABLE users ADD COLUMN approved INT NOT NULL DEFAULT 0;
 ALTER TABLE users ALTER COLUMN approved DROP DEFAULT;
+
+CREATE INDEX ON users (email_valid);
+CREATE INDEX ON users (approved);
