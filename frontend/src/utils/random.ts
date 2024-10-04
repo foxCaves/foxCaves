d6e3a36e7626f554759af81638a01697f6c08b10
Mark Dietzer <git@doridian.net>
2024-10-04 09:23:54 -0700

    Upgrade eslint-plugin-unicorn to v56 and use globalThis everywhere to fix lint (#1314)
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- frontend/src/utils/random.ts
+++ frontend/src/utils/random.ts
@@ -1,7 +1,7 @@
 const RNG_ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 export function randomString(length = 10): string {
     const u8 = new Uint8Array(length);
-    window.crypto.getRandomValues(u8);
+    globalThis.crypto.getRandomValues(u8);
     let result = '';
     for (let i = 0; i < length; i++) {
         // This algorithm is NOT crypto secure, but it is good enough for our purposes
