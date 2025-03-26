2071ba0b844ec37e5a8953db12426ba438eeb41d
Doridian <git@doridian.net>
2025-03-25 20:05:13 -0700

    unk

--- config/example.lua
+++ config/example.lua
@@ -34,7 +34,7 @@ return {
         max_past = 1,
         max_future = 1,
         secret_bytes = 20,
-        issuer = "foxCaves DEV"
+        issuer = "foxCaves EXAMPLE"
     },
     captcha = {
         registration = false,
