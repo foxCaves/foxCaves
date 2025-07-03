9f284e8232cecd996c48993c8f45145e406047a5
Doridian <git@doridian.net>
2025-07-03 16:39:39 -0700

    Stop clobbering around in pgmoon

--- backend/modules/foxcaves/migrator.lua
+++ backend/modules/foxcaves/migrator.lua
@@ -11,6 +11,14 @@ local error = error
 local M = {}
 require('foxcaves.module_helper').setmodenv()
 
+local function db_query_err(db, query)
+    local res, qerr = db:query(query)
+    if not res then
+        error(qerr)
+    end
+    return res
+end
+
 local function process_migration_dir(db, ran_migrations, dir)
     local file_array = {}
     for file in lfs.dir(dir) do
@@ -34,7 +42,7 @@ local function process_migration_dir(db, ran_migrations, dir)
 
         local migration_query =
             'BEGIN;\n' .. data .. 'INSERT INTO migrations (name) VALUES (' .. db:escape_literal(file) .. ');\nCOMMIT;'
-        db:query_err(migration_query)
+        db_query_err(db, migration_query)
     end
 end
 local function setup_db()
@@ -44,17 +52,9 @@ local function setup_db()
         error(err)
     end
 
-    function db:query_err(query)
-        local res, qerr = self:query(query)
-        if not res then
-            error(qerr)
-        end
-        return res
-    end
+    db_query_err(db, 'CREATE TABLE IF NOT EXISTS migrations (name VARCHAR(255) PRIMARY KEY);')
 
-    db:query_err('CREATE TABLE IF NOT EXISTS migrations (name VARCHAR(255) PRIMARY KEY);')
-
-    local ran_migrations_arr = db:query_err('SELECT name FROM migrations;')
+    local ran_migrations_arr = db_query_err(db, 'SELECT name FROM migrations;')
     local ran_migrations = {}
     for _, row in ipairs(ran_migrations_arr) do
         ran_migrations[row.name] = true
