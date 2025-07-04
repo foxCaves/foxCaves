a574fce2964ef55fe0ef5e209a1a0fd3865557ea
Doridian <git@doridian.net>
2025-07-04 09:28:14 -0700

    Ensure we are always autoloaded

--- backend/nginx_init.lua
+++ backend/nginx_init.lua
@@ -2,5 +2,7 @@ local path = require('path')
 local root = path.abs(debug.getinfo(1, 'S').source:sub(2):match('(.*/)'))
 dofile(root .. '/init.lua')
 
+require('foxcaves.registry').autoload()
+
 local hooks = require('foxcaves.hooks')
 hooks.call('ngx_init')
