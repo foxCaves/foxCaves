91ff6546a275eb96aba6de3c811187e8376490c7
Doridian <git@doridian.net>
2025-07-03 19:40:44 -0700

    f

--- backend/routes/api/v1/system/info.lua
+++ backend/routes/api/v1/system/info.lua
@@ -1,8 +1,6 @@
 local env = require('foxcaves.env')
 local revision = require('foxcaves.revision')
 local enable_sentry = not not require('foxcaves.config').sentry.dsn
-local utils = require('foxcaves.utils')
-local ngx = ngx
 
 R.register_route('/api/v1/system/info', 'GET', R.make_route_opts_anon(), function()
     return {
