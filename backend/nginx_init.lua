12214b14b914068e114fcd9e8a13cca79bfe8937
Doridian <git@doridian.net>
2025-07-04 17:39:13 -0700

    Auto append _EMPTY suffix to empty tables

--- backend/nginx_init.lua
+++ backend/nginx_init.lua
@@ -9,7 +9,7 @@ dofile(root .. '/includes/autoloader.lua')
 local executor_name = require('foxcaves.config').app.executor
 ngx.log(ngx.NOTICE, 'Using executor: ', executor_name)
 local executor = dofile(root .. '/includes/executors/' .. executor_name .. '.lua')
-setfenv(executor, require('foxcaves.module_helper').make_empty_table('EXECUTOR_EMPTY'))
+setfenv(executor, require('foxcaves.module_helper').make_empty_table('EXECUTOR'))
 rawset(_G, 'foxcaves_executor', executor)
 
 local hooks = require('foxcaves.hooks')
