6f794135abf8c32b799ab75b05c0991104e2e7e5
Doridian <git@doridian.net>
2025-10-27 23:07:14 -0700

    collapse migrations

--- backend/migrations/20200831005722_setup.sql
+++ backend/migrations/20200831005722_setup.sql
@@ -3,11 +3,13 @@ CREATE TABLE users (
     username VARCHAR(255),
     email VARCHAR(255),
     password VARCHAR(255),
-    active INT NOT NULL,
+    email_valid INT NOT NULL,
     storage_quota BIGINT NOT NULL,
     security_version INT NOT NULL,
     api_key VARCHAR(255),
     admin INT NOT NULL DEFAULT 0,
+    approved INT NOT NULL,
+    totp_secret VARCHAR(255) NOT NULL,
     created_at timestamp,
     updated_at timestamp
 );
@@ -42,7 +44,7 @@ CREATE INDEX `size` ON files(`size`);
 CREATE TABLE links (
     id VARCHAR(32) PRIMARY KEY,
     owner UUID REFERENCES users (id),
-    url VARCHAR(4096),
+    target VARCHAR(4096),
     created_at timestamp,
     updated_at timestamp,
     expires_at timestamp
@@ -53,3 +55,20 @@ CREATE INDEX `owner` ON links(`owner`);
 CREATE INDEX `created_at` ON links(`created_at`);
 CREATE INDEX `updated_at` ON links(`updated_at`);
 CREATE INDEX `expires_at` ON links(`expires_at`);
+
+
+CREATE TABLE news (
+    id UUID PRIMARY KEY,
+    title TEXT,
+    content TEXT,
+    author UUID REFERENCES users(id),
+    editor UUID NULL REFERENCES users(id),
+    created_at timestamp,
+    updated_at timestamp
+);
+
+CREATE INDEX `author` ON news(`author`);
+CREATE INDEX `editor` ON news(`editor`);
+
+CREATE INDEX `created_at` ON news(`created_at`);
+CREATE INDEX `updated_at` ON news(`updated_at`);
--- backend/migrations/20240410231711_migrate_users_email_valid.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE users RENAME COLUMN active TO email_valid;
-ALTER TABLE users ADD COLUMN approved INT NOT NULL DEFAULT 0;
-ALTER TABLE users ALTER COLUMN approved DROP DEFAULT;
-
-CREATE INDEX email_valid ON users(email_valid);
-CREATE INDEX approved ON users(approved);
