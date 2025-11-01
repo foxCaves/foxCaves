931fcf29721496fd05b2e6aee7b8e5c517d3393a
Doridian <git@doridian.net>
2025-11-01 14:48:24 -0700

    root

--- backend/routes/healthz.lua
+++ backend/routes/healthz.lua
@@ -1,7 +1,3 @@
 R.register_route('/healthz', 'GET', R.make_route_opts_anon(), function()
     return { health = 'OK' }
 end)
-
-R.register_route('/readyz', 'GET', R.make_route_opts_anon(), function()
-    return { ready = 'OK' }
-end)
