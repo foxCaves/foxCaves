9b7e3e6dc711ed518ee55dcccbdc7db2372db06e
Doridian <git@doridian.net>
2025-11-01 10:34:28 -0700

    fmt

--- backend/modules/foxcaves/config.lua
+++ backend/modules/foxcaves/config.lua
@@ -1,16 +1,14 @@
 local env = require('foxcaves.env')
 local CONFIG_ROOT = require('path').abs(require('foxcaves.consts').ROOT .. '/config')
 
-local config_env = {
-    os = { getenv = os.getenv },
-}
-
 local function load_config_file(name)
     local func, err = loadfile(CONFIG_ROOT .. '/' .. name .. '.lua')
     if not func then
         error('Failed loding config: ' .. err)
     end
-    func = setfenv(func, config_env)
+    func = setfenv(func, {
+        os = { getenv = os.getenv },
+    })
     return func()
 end
 
