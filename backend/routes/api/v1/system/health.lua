7116d461cc4f281610dd827495bd0982a452a981
Doridian <git@doridian.net>
2025-11-01 14:43:55 -0700

    ready route

--- backend/routes/api/v1/system/health.lua
+++ backend/routes/api/v1/system/health.lua
@@ -1,3 +1,7 @@
 R.register_route('/api/v1/system/health', 'GET', R.make_route_opts_anon(), function()
     return { health = 'OK' }
 end)
+
+R.register_route('/api/v1/system/ready', 'GET', R.make_route_opts_anon(), function()
+    return { ready = 'OK' }
+end)
